05941ea91679e513064081eba27bdeb70a0cc166
asishprodhan <asihpradhan429@gmail.com>
2025-10-09 20:41:00 +0530

    redeploying

--- index.js
+++ index.js
@@ -70,7 +70,7 @@ if (joinForm) {
     };
 
     try {
-      const res = await fetch("https://local-event-app.onrender.com", {
+      const res = await fetch("https://local-event-app.onrender.com/join-event", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
